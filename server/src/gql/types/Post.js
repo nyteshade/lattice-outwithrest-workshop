11790d07985c0338801b2975e905e0a3853b2e4c
Brielle Harrison <nyteshade@gmail.com>
2017-11-04 00:01:10 -0500

    A post with a single working query

--- server/src/gql/types/Post.js
+++ server/src/gql/types/Post.js
@@ -1,6 +1,5 @@
-import { GQLBase, Schema, Properties, resolver, mutator } from 'graphql-lattice'
-import { User } from './User'
-import { POSTS, USERS } from '../../urls'
+import { GQLBase, Schema, Properties, resolver } from 'graphql-lattice'
+import { POSTS } from '../../urls'
 
 import Axios from 'axios'
 
@@ -10,42 +9,18 @@ import Axios from 'axios'
     userId: ID
     title: String
     body: String
-    user: User
   }
 
   type Query {
-    findPost(id: ID): Post
     getAllPosts: [Post]
-  }
-
-  type Mutation {
-    createPost(title: String, body: String, userId: ID): Post
   }
 `)
 @Properties(['postId', 'id'], 'title', 'body')
 export class Post extends GQLBase {
-  async user() {
-    let { data } = await Axios.get(`${USERS}/${this.model.userId}`)
-
-    return new User(data)
-  }
-
-  @resolver async findPost(requestData, {id}) {
-    let { data } = await Axios.get(`${POSTS}/${id}`)
-
-    return new Post(data, requestData)
-  }
-
   @resolver async getAllPosts(requestData) {
     let { data } = await Axios.get(POSTS)
     let posts = data.map(model => new Post(model, requestData))
 
     return posts
-  }
-
-  @mutator async createPost(requestData, params) {
-    let { data } = await Axios.post(POSTS, params)
-
-    return new Post(data, requestData)
   }
 }
